66a0f7271dceb3b6fc29229bc9607cb69c232289
Sabeek Ahamed S <86552906+sabeekahamed@users.noreply.github.com>
2022-05-27 23:29:55 +0530

    Time: 3 ms (34.03%), Space: 5.9 MB (74.11%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -5,7 +5,7 @@ public:
         while(num>0)
         {
             if( num % 2 ==0)
-                num = num /2;
+                num = num >> 1;
             else
                 num -=1;
             count++;
