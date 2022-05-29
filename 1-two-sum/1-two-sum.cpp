30cd5267b69cf4331a5ef0cbeadf701ef63047bf
Sabeek Ahamed S <86552906+sabeekahamed@users.noreply.github.com>
2022-05-29 16:25:57 +0530

    Time: 19 ms (59.18%), Space: 11 MB (24.09%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        //O(n^2)
+        /*
         for(int i=0;i<nums.size();i++)
         {
             int curValue = target - nums[i];
@@ -11,5 +13,21 @@ public:
             }
         }
         return vector<int>({});
+        */
+        //search in un ordered map is (1)
+        // we need to perform search operation in maximum n times 
+        //so time complexity is O(N) and space complexity is O(N) (map)
+        unordered_map<int,int> valueMap(nums.size());
+        for(int i=0;i<nums.size();i++)
+        {
+            int curValue = target - nums[i];
+            auto itr= valueMap.find(curValue);
+            if( itr != valueMap.end())
+                return vector<int>({i,itr->second});
+            else
+                valueMap.insert(pair<int,int>(nums[i],i));
+        }
+        return vector<int>();
+        
     }
 };
