0e266f64f1e727d327977945693270e61e49bbf4
Sabeek Ahamed S <86552906+sabeekahamed@users.noreply.github.com>
2022-05-29 16:51:52 +0530

    Time: 6 ms (28.23%), Space: 9 MB (90.35%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    /*
+         nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+            1,2,3,0,0,0
+            2,5,6
+            st1)
+                1,2,3,0,0,6
+                  2,5
+            st2)
+                1,2,3,0,5,6
+                    2
+            st3)
+                1,2,3,3,5,6
+                  2
+            st4)
+                1,2,2,3,5,6
+         
+    */
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int i=m-1,j=n-1,k=m+n-1;
         while(i>=0&&j>=0)
